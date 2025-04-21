372407fed70b800b04622c6020a2c86b9e806151
Ioan-Tudor-Alexandru Anghel <ioan.anghel123@gmail.com>
2025-04-21 14:16:31 +0300

    Made mofifications for monitoring and implemented metrics

---
+++ src/main/java/ro/unibuc/hello/config/MetricsConfig.java
@@ -0,0 +1,16 @@
+package ro.unibuc.hello.config;
+
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "booking-service");
+    }
+}
